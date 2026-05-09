70c5a9740b524ba8f24622958ccf1f48a27c649e
king mike <mikesamuel387@gmail.com>
2026-05-09 03:10:29 +0300

    Remove payment routes from paymentRoutes.js

--- paymentRoutes.js
+++ paymentRoutes.js
@@ -1,8 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const paymentController = require("../controllers/paymentController");
 
-router.post("/deposit", paymentController.deposit);
-router.post("/callback", paymentController.callback);
-
-module.exports = router;
