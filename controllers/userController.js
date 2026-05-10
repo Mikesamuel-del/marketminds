db7f514eef1dd9c9568dd10801196e38cd153f89
king mike <mikesamuel387@gmail.com>
2026-05-10 17:27:32 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -591,7 +591,8 @@ const forgotPassword = async (req, res) => {
     user.resetPasswordExpire = new Date(Date.now() + 60 * 60 * 1000);
     await user.save({ validateModifiedOnly: true });
 
-    const frontend = process.env.FRONTEND_URL || "http://localhost:3000";
+    const frontend = process.env.FRONTEND_URL || "dashboard-iota-woad-18.vercel.app
+";
     const resetUrl = `${String(frontend).replace(/\/$/, "")}/reset-password/${resetToken}`;
 
     const result = await sendMail({
